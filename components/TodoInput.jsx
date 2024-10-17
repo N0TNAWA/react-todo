429c3fd1ca52ce44823aab1ff62175e0fc34aca6
N0TNAWA <btorpsamuel@gmail.com>
2024-10-17 23:43:36 +0200

    Added new files

---
+++ components/TodoInput.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export default function TodoInput(props) {
+  const { handleAddTodo, todoValue, setTodoValue } = props
+
+  return(
+    <header>
+
+     <input value={todoValue} onChange={(e) => {
+       setTodoValue(e.target.value)
+     }} placeholder="Enter task..."/>
+
+     <button onClick={() => {
+       handleAddTodo(todoValue) 
+       setTodoValue('')
+     }}>Enter</button>
+
+    </header>
+  )
+}
